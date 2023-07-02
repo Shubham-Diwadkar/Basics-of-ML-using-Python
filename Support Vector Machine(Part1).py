549c8a4fa4a8a2246b522fa9086b75b1b7b6acb7
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-07-02 22:19:02 +0530

    Update Support Vector Machine(Part1).py

--- Support Vector Machine(Part1).py
+++ Support Vector Machine(Part1).py
@@ -1,16 +1,27 @@
-# Program for 1 column
+# Program for implementing Support Vector Machine on 1 column 
 
+# Importing Libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file_path = r"D:\Python Programing\All Datasets\cancer.csv"
+# Giving the file path to access the cancer.csv file
+file_path = r"YOUR_FILE_PATH\cancer.csv"
+
+# Reading the .csv file
 x = pd.read_csv(file_path)
+
+# Converting DataFrame(x) into numpy array
 a = np.array(x)
+
+# Accessing all the data of rows starting starting from 30th column 
 y = a[:, 30]
 
+# Stacking all the values of State column in a numpy array
 x = np.column_stack((x.State))
 
+# Defining the shape of all stacked data values
 x.shape
 
-print(x),(y)
+# Prints all the values of stacked data values and values of y
+print("\n", x),("\n", y)
